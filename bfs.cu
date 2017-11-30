4513469d41309627b901a3b81a2e92e16405b0a3
Muvaffak Onus <onus.muvaffak@gmail.com>
2017-11-30 20:34:03 +0300

    karate is working. recursive implementation. copapers and bigger is not working

--- bfs.cu
+++ bfs.cu
@@ -6,6 +6,8 @@
 #include "graphio.h"
 #include "graph.h"
 
+const int N = 512;
+
 char gfile[2048];
 
 using namespace::std;
@@ -22,12 +24,10 @@ void random_ints(int* a, int N)
 }
 // CUDA STARTS
 
-#define N (2048*2048)
-#define M 512
 
 
 __global__
-void run(etype *row_ptr, vtype *col_ind, int nov);
+void run(etype *row_ptr, vtype *col_ind, etype *results, int nov);
 
 
 /*
@@ -53,23 +53,47 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
   /****** YOUR CODE GOES HERE *******/
+  if (nov > N) {
+    nov = N;
+  }
   etype *d_row_ptr;
   vtype *d_col_ind;
-  int size = nov * sizeof(int);
 
-  cudaMalloc((void **)&d_row_ptr, size);
-  cudaMalloc((void **)&d_col_ind, size);
+  etype *d_results;
+  int row_size = nov * sizeof(etype);
+  int col_size = row_ptr[nov-1] * sizeof(vtype);
 
-  cudaMemcpy(d_row_ptr, row_ptr, size, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_col_ind, col_ind, size, cudaMemcpyHostToDevice);
+  cudaMalloc((void **)&d_row_ptr, row_size);
+  cudaMalloc((void **)&d_col_ind, col_size);
+  cudaMalloc((void **)&d_results, row_size);
 
-  run<<<(N + M - 1)/M, M>>>(d_row_ptr, d_col_ind, nov);
+  cudaMemcpy(d_row_ptr, row_ptr, row_size, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_col_ind, col_ind, col_size, cudaMemcpyHostToDevice);
 
-  //cudaMemcpy(c, dc, size, cudaMemcpyDeviceToHost);
+  printf("%s %d\n", "nov is", nov);
+  run<<<(nov + 511)/512, 512>>>(d_row_ptr, d_col_ind, d_results, nov);
 
-  // for (size_t i = 0; i < N; i++) {
-  //   cout << c[i] << endl;
-  // }
+  etype *results = (etype *) malloc(row_size);
+  cudaMemcpy(results, d_results, row_size, cudaMemcpyDeviceToHost);
+
+
+  unsigned int max = 0;
+  int maxIndex = -1;
+
+  unsigned int totalNonZero = 0;
+  for (unsigned int i = 0; i < nov; i++) {
+    if (results[i] == 0) {
+      continue;
+    }
+    totalNonZero++;
+    if(results[i] >= max){
+      max = results[i];
+      maxIndex = i;
+    }
+  }
+  cout << "total non zeroes " << totalNonZero << endl;
+  printf("%s %d %s%d\n", "Min closeness belongs to", maxIndex, "with score of 1/", max);
+
   cudaFree(d_row_ptr);
   cudaFree(d_col_ind);
   //cudaFree(dc);
--- cudabfs.cu
+++ cudabfs.cu
@@ -1,6 +1,44 @@
 #include <stdio.h>
 #include "graph.h"
+#include <queue>
+
+//etype unsigned int
+
+__device__
+void recur(etype vertice, etype level, etype *visited, etype *row_ptr, vtype *col_ind, etype *results, int nov){
+  if (vertice == nov-1) {
+    return;
+  }
+  for (size_t i = row_ptr[vertice]; i < row_ptr[vertice+1]; i++) {
+    etype n_ind = col_ind[i];
+    if (visited[n_ind] == 0) {
+      visited[n_ind] = level;
+    }
+  }
+  for (size_t i = row_ptr[vertice]; i < row_ptr[vertice+1]; i++) {
+    etype n_ind = col_ind[i];
+    if (visited[n_ind] == level) {
+      recur(n_ind, level+1, visited, row_ptr, col_ind, results, nov);
+    }
+  }
+}
+
 __global__
-void run(etype *row_ptr, vtype *col_ind, int nov) {
+void run(etype *row_ptr, vtype *col_ind, etype *results, int nov) {
   int index = threadIdx.x + blockIdx.x * blockDim.x;
+  if (index < nov) {
+    etype *visited = new etype[nov];
+    for (size_t i = 0; i < nov; i++) {
+      visited[i] = 0;
+    }
+
+    recur(index, 1, visited, row_ptr, col_ind, results, nov);
+
+    etype distanceSum = 0;
+    for (size_t i = 0; i < nov; i++) {
+      distanceSum += visited[i];
+    }
+    results[index] = distanceSum;
+    //printf("vertice %d has closeness of %d\n", index, distanceSum);
+  }
 }
